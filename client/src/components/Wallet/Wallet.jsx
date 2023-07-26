45a783f100bbe440290fcd191e2faa3642d2c82f
anish <anishkumarpatro03@gmail.com>
2023-07-26 23:01:21 +0530

    metamask for mobile

--- client/src/components/Wallet/Wallet.jsx
+++ client/src/components/Wallet/Wallet.jsx
@@ -5,17 +5,17 @@ import "./Wallet.css";
 
 const Wallet = ({ saveState }) => {
   const [connected, setConnected] = useState(false);
+  const isAndroid = /android/i.test(navigator.userAgent);
   const init = async () => {
     try {
       const web3 = new Web3(window.ethereum);
       await window.ethereum.request({ method: "eth_requestAccounts" });
       const contract = new web3.eth.Contract(
-        ABI,  
+        ABI,
         "0xc6E21997d91D00677fE691Ce6cFAd387549688A5"
       );
       setConnected(true);
       saveState({ web3: web3, contract: contract });
-      
     } catch (error) {
       alert("Please Install Metamask");
     }
@@ -24,8 +24,15 @@ const Wallet = ({ saveState }) => {
   return (
     <>
       <div className="header">
+        {isAndroid && (
+          <button className="connectBTN">
+            <a href="https://anish03-portfolio.netlify.app/">
+              Click For Mobile
+            </a>
+          </button>
+        )}
         <button className="connectBTN" onClick={init} disabled={connected}>
-          {!connected ? "Connect Metamask": "Connected"}
+          {!connected ? "Connect Metamask" : "Connected"}
         </button>
       </div>
     </>
--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,31 +1,25 @@
-import {useState,useEffect} from "react";
-import './Contact.css'
+import { useState, useEffect } from "react";
+import "./Contact.css";
 
-const Contact = ({state}) => {
-    const [resume,setResume]=useState("");
-    useEffect(()=>{
-        const {contract}=state;
-        const resumeDetails=async()=>{
-            const resumeCid = await contract.methods.resumeLink().call();
-            setResume("https://gateway.pinata.cloud/ipfs/"+resumeCid);
-        }
-        contract && resumeDetails();
-    },[state])
-    
-    return (
-        <section className="contact-section">
-            <h1 className="title">
-                Interested?
-                Let's Get In Touch!
-            </h1>
-            <a href={resume} target='_blank' rel="noopener noreferrer">
-                <button className="downlodeBTN">
-                    View Resume
-                </button>
-            </a>
+const Contact = ({ state }) => {
+  const [resume, setResume] = useState("");
+  useEffect(() => {
+    const { contract } = state;
+    const resumeDetails = async () => {
+      const resumeCid = await contract.methods.resumeLink().call();
+      setResume("https://gateway.pinata.cloud/ipfs/" + resumeCid);
+    };
+    contract && resumeDetails();
+  }, [state]);
 
-        </section>
-    )
-}
+  return (
+    <section className="contact-section">
+      <h1 className="title">Interested? Let's Get In Touch!</h1>
+      <a href={resume} target="_blank" rel="noopener noreferrer">
+        <button className="downlodeBTN">View Resume</button>
+      </a>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
